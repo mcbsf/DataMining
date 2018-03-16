0e2640a8522c6b90aba43de80ca2f955905d3f56
mcbsf <mcbsf@cin.ufpe.br>
2018-03-16 10:38:42 -0300

    correção de main e JSON no formato txt

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import urllib.request, json 
 
 #pega a o json dentro da url e bota todos os tópicos mais populares dentro da variável "data"
-with urllib.request.urlopen("http://reddit.com/r/Bitcoin/.json") as url:
-    data = json.loads(url.read())['data']['children']
+# with urllib.request.urlopen("http://reddit.com/r/Bitcoin/.json") as url:
+#     data = json.loads(url.read())['data']['children']
+
+
+# with open('data.txt', 'w') as outfile:  
+#     json.dump(data, outfile)
 
 urls = []
+topics_id = []
+counter = 0
+
+with open('data.txt') as json_file:  
+    data = json.load(json_file)
+
 #pega a url de acesso aos tópicos
 for topic in data:
-	urls.append(topic["data"]["url"])
+	urls.append("http://reddit.com"+topic["data"]["permalink"])
+	subrredits_id.append(topic["data"]["id"])
 
 for url in urls:
 	print(url)
+	print(topics_id[counter])
+	counter = counter + 1
+	
+	with urllib.request.urlopen(url) as url_aux:
+    	data = json.loads(url_aux.read())['data']['children']
+    	
+    	with open('comment_table.txt', 'w') as outfile:  
+	    	json.dump(data, outfile)	
+
+
+
+comment_table = []
+
