e5144be85ded3c382862b58534425477dbc1bfbe
mcbsf <mcbsf@cin.ufpe.br>
2018-03-27 19:28:40 -0300

    criando analisador e classificador de postag

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import urllib.request, json 
+from analyser import SentimentAnalyser
 
 #pega a o json dentro da url e bota todos os tópicos mais populares dentro da variável "data"
 # with urllib.request.urlopen("http://reddit.com/r/Bitcoin/.json") as url:
@@ -7,24 +8,24 @@ import urllib.request, json
 
 # with open('data.txt', 'w') as outfile:  
 #     json.dump(data, outfile)
+#AQUIIIIIIIII
+# urls = []
+# topics_id = []
+# counter = 0
 
-urls = []
-topics_id = []
-counter = 0
+# with open('data.txt') as json_file:  
+#     data = json.load(json_file)
 
-with open('data.txt') as json_file:  
-    data = json.load(json_file)
+# #pega a url de acesso aos tópicos
+# for topic in data:
+# 	urls.append("http://reddit.com"+topic["data"]["permalink"])
+# 	topics_id.append(topic["data"]["id"])
 
-#pega a url de acesso aos tópicos
-for topic in data:
-	urls.append("http://reddit.com"+topic["data"]["permalink"])
-	topics_id.append(topic["data"]["id"])
-
-for url in urls:
-	print(url)
-	print(topics_id[counter])
-	counter = counter + 1
-	
+# for url in urls:
+# 	print(url)
+# 	print(topics_id[counter])
+# 	counter = counter + 1
+#AQUIIIIIIIIII	
 #INÍCIO DO CÒDIGO PARA COMEÇAR A MONTAR A TABELA DE COMENTÁRIOS	
 	# with urllib.request.urlopen(url) as url_aux:
  #    	data = json.loads(url_aux.read())['data']['children']
@@ -32,7 +33,8 @@ for url in urls:
  #    	with open('comment_table.txt', 'w') as outfile:  
 	#     	json.dump(data, outfile)	
 
-
+analyser = SentimentAnalyser()
+print(analyser.analyseSentence('hate hate, love happy none'))
 
 comment_table = []
 
