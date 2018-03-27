c2128a77d720cac3ef69b34e3a8add3480518b30
mcbsf <mcbsf@cin.ufpe.br>
2018-03-27 19:29:44 -0300

    tirando as linhas de comentários pro main rodar normal

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ from analyser import SentimentAnalyser
 
 # with open('data.txt', 'w') as outfile:  
 #     json.dump(data, outfile)
-#AQUIIIIIIIII
-# urls = []
-# topics_id = []
-# counter = 0
 
-# with open('data.txt') as json_file:  
-#     data = json.load(json_file)
+urls = []
+topics_id = []
+counter = 0
 
-# #pega a url de acesso aos tópicos
-# for topic in data:
-# 	urls.append("http://reddit.com"+topic["data"]["permalink"])
-# 	topics_id.append(topic["data"]["id"])
+with open('data.txt') as json_file:  
+    data = json.load(json_file)
 
-# for url in urls:
-# 	print(url)
-# 	print(topics_id[counter])
-# 	counter = counter + 1
-#AQUIIIIIIIIII	
+#pega a url de acesso aos tópicos
+for topic in data:
+	urls.append("http://reddit.com"+topic["data"]["permalink"])
+	topics_id.append(topic["data"]["id"])
+
+for url in urls:
+	print(url)
+	print(topics_id[counter])
+	counter = counter + 1
+	
 #INÍCIO DO CÒDIGO PARA COMEÇAR A MONTAR A TABELA DE COMENTÁRIOS	
 	# with urllib.request.urlopen(url) as url_aux:
  #    	data = json.loads(url_aux.read())['data']['children']
